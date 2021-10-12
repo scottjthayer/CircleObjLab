5bf524dc121b8c04b09dc00018a1a9e72047e4ab
scott <scottjthayer@gmail.com>
2021-10-11 23:54:25 -0400

    first commit for Circle Obj Lab

---
+++ Circle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Circle_Objects_Lab
+{
+    class Circle
+    {
+
+        public double Radius;
+        
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        private double radius
+        {
+            get { return Radius; }      
+        }
+        
+        public double CalculateCircumference()
+        {
+           
+            double circumference = 2 * Math.PI * radius;
+            return circumference;
+        }
+        public string CalculateFormattedCircumference()
+        {
+            double circumference = CalculateCircumference();
+            string cirForm = FormatNumber(circumference);
+            return cirForm;
+        }
+
+        public double CalculateArea()
+        {
+
+            double area = Math.PI * (radius * radius);
+            return area;
+        }
+
+        public string CalculateFormattedArea()
+        {
+            double area = CalculateArea();
+            string areaForm = FormatNumber(area);
+            return areaForm;
+        }
+
+        private string FormatNumber(double x)
+        {
+            string format = string.Format("{0:0.00}", x);
+            return format;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Circle_Objects_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Circle> numCircles = new List<Circle>();
+            bool runner = true;
+
+            while (runner == true)
+            {
+                double radius = 0;
+                radius = ValidRadius(0);
+                Circle circle = new Circle(radius);
+                Console.WriteLine($"Circumference: {circle.CalculateFormattedCircumference()}");
+                Console.WriteLine($"Area:          {circle.CalculateFormattedArea()}");
+                numCircles.Add(circle);
+                runner = GetYesOrNo("Would you like to calculate another circle?");
+            }
+            Console.WriteLine($"Goodbye. You created {numCircles.Count} Circle objects");
+        }
+        public static bool GetYesOrNo(string prompt)
+        {
+            while (true)
+            {
+                Console.WriteLine(prompt);
+                Console.Write("Y/N? ");
+                string input = Console.ReadLine().Trim().ToLower();
+                if (input == "y")
+                {
+                    return true;
+                }
+                else if (input == "n")
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("That input is not valid, please try again.");
+                }
+            }
+        }
+        public static double ValidRadius(double x)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter radius: ");
+                    x = double.Parse(Console.ReadLine());
+                    if (x <= 0)
+                    {
+                        Console.WriteLine("Needs to be greater than 0");
+                        continue;
+                    }
+                    break;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("That was not a number.");
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return x;
+
+        }
+    }
+}
